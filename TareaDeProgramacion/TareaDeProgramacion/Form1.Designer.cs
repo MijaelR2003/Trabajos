00e337d10926b1ff2234341912bca1188d310a77
MijaelR2003 <61435788+MijaelR2003@users.noreply.github.com>
2021-10-13 19:39:31 -0400

    Hola
    Hola

---
+++ TareaDeProgramacion/TareaDeProgramacion/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace TareaDeProgramacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.btnVerEjercicio = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtejercicio = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.BackColor = System.Drawing.Color.Red;
+            this.btnSalir.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnSalir.Location = new System.Drawing.Point(431, 300);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(65, 33);
+            this.btnSalir.TabIndex = 1;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = false;
+            this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
+            // 
+            // btnVerEjercicio
+            // 
+            this.btnVerEjercicio.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnVerEjercicio.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.btnVerEjercicio.Location = new System.Drawing.Point(37, 272);
+            this.btnVerEjercicio.Name = "btnVerEjercicio";
+            this.btnVerEjercicio.Size = new System.Drawing.Size(118, 35);
+            this.btnVerEjercicio.TabIndex = 4;
+            this.btnVerEjercicio.Text = "Ver Ejercicio";
+            this.btnVerEjercicio.UseVisualStyleBackColor = false;
+            this.btnVerEjercicio.Click += new System.EventHandler(this.btnVerEjercicio_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Historic", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(86, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(337, 32);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ejercicios en Windows Form";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(37, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(320, 25);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ingrese que ejercicio quiere ver :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(37, 142);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(318, 50);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Ingresa: Ejercicio 1, Ejercicio 2... \r\nHasta el ejercicio que desea ver ";
+            // 
+            // txtejercicio
+            // 
+            this.txtejercicio.Font = new System.Drawing.Font("Segoe UI Symbol", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.txtejercicio.Location = new System.Drawing.Point(37, 215);
+            this.txtejercicio.Name = "txtejercicio";
+            this.txtejercicio.Size = new System.Drawing.Size(183, 33);
+            this.txtejercicio.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 348);
+            this.Controls.Add(this.txtejercicio);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnVerEjercicio);
+            this.Controls.Add(this.btnSalir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.Button btnVerEjercicio;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtejercicio;
+    }
+}
+
---
+++ TareaDeProgramacion/TareaDeProgramacion/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TareaDeProgramacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+             Para poder crear una pestaña creas como una clase pero en vez de una clase un Wndows Form 
+            */
+            Bitmap img = new Bitmap(Application.StartupPath + @"/img/fondo2.jpg");
+            this.BackgroundImage = img;
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+        private void btnVerEjercicio_Click(object sender, EventArgs e)
+        {
+            switch (txtejercicio.Text) {
+                case "Ejercicio 1":
+                    Form formulario2 = new Form2();
+                    formulario2.Show();
+                    break;
+                case "Ejercicio 2":
+                    Form formulario3 = new Form3();
+                    formulario3.Show();
+                    break;
+                case "Ejercicio 3":
+                    Form formulario4 = new Form4();
+                    formulario4.Show();
+                    break;
+                case "Ejercicio 4":
+                    Form formulario5 = new Form5();
+                    formulario5.Show();
+                    break;
+                case "Ejercicio 5":
+                    Form formulario6 = new Form6();
+                    formulario6.Show();
+                    break;
+                case "Ejercicio 6":
+                    Form formulario7 = new Form7();
+                    formulario7.Show();
+                    break;
+                case "Ejercicio 7":
+                    Form formulario8 = new Form8();
+                    formulario8.Show();
+                    break;
+                case "Ejercicio 8":
+                    Form formulario9 = new Form9();
+                    formulario9.Show();
+                    break;
+                case "Ejercicio 9":
+                    Form formulario10 = new Form10();
+                    formulario10.Show();
+                    break;
+                case "Ejercicio 10":
+                    Form formulario11 = new Form11();
+                    formulario11.Show();
+                    break;
+            }
+        }
+
+        private void btnSalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
+
