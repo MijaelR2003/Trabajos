2437cf76a74dc72bb0c7e30a868261042daf0131
MijaelR2003 <61435788+MijaelR2003@users.noreply.github.com>
2021-11-06 21:20:12 -0400

    LoginDeProgramacion

---
+++ ProyectoDeProgramacionyBaseDeDatos/ProyectoDeProgramacionyBaseDeDatos/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace ProyectoDeProgramacionyBaseDeDatos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblerror = new System.Windows.Forms.Label();
+            this.txtusuarios = new System.Windows.Forms.TextBox();
+            this.txtcontrasena = new System.Windows.Forms.TextBox();
+            this.btningresar = new System.Windows.Forms.Button();
+            this.btnsalir = new System.Windows.Forms.Button();
+            this.btnvercontra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 27.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(371, 50);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Consultorio Dental";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(31, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Usuario";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.label3.Location = new System.Drawing.Point(3, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 24);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Contraseña";
+            // 
+            // lblerror
+            // 
+            this.lblerror.AutoSize = true;
+            this.lblerror.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.lblerror.Location = new System.Drawing.Point(12, 192);
+            this.lblerror.Name = "lblerror";
+            this.lblerror.Size = new System.Drawing.Size(34, 24);
+            this.lblerror.TabIndex = 3;
+            this.lblerror.Text = "----";
+            // 
+            // txtusuarios
+            // 
+            this.txtusuarios.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txtusuarios.Location = new System.Drawing.Point(115, 106);
+            this.txtusuarios.Name = "txtusuarios";
+            this.txtusuarios.Size = new System.Drawing.Size(196, 29);
+            this.txtusuarios.TabIndex = 4;
+            // 
+            // txtcontrasena
+            // 
+            this.txtcontrasena.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.txtcontrasena.Location = new System.Drawing.Point(115, 146);
+            this.txtcontrasena.Name = "txtcontrasena";
+            this.txtcontrasena.PasswordChar = '*';
+            this.txtcontrasena.Size = new System.Drawing.Size(196, 29);
+            this.txtcontrasena.TabIndex = 5;
+            // 
+            // btningresar
+            // 
+            this.btningresar.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btningresar.Location = new System.Drawing.Point(115, 227);
+            this.btningresar.Name = "btningresar";
+            this.btningresar.Size = new System.Drawing.Size(95, 32);
+            this.btningresar.TabIndex = 6;
+            this.btningresar.Text = "Ingresar";
+            this.btningresar.UseVisualStyleBackColor = true;
+            this.btningresar.Click += new System.EventHandler(this.btningresar_Click);
+            // 
+            // btnsalir
+            // 
+            this.btnsalir.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnsalir.Location = new System.Drawing.Point(277, 227);
+            this.btnsalir.Name = "btnsalir";
+            this.btnsalir.Size = new System.Drawing.Size(80, 32);
+            this.btnsalir.TabIndex = 7;
+            this.btnsalir.Text = "Salir ";
+            this.btnsalir.UseVisualStyleBackColor = true;
+            this.btnsalir.Click += new System.EventHandler(this.btnsalir_Click);
+            // 
+            // btnvercontra
+            // 
+            this.btnvercontra.Font = new System.Drawing.Font("Franklin Gothic Book", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.btnvercontra.Location = new System.Drawing.Point(402, 150);
+            this.btnvercontra.Name = "btnvercontra";
+            this.btnvercontra.Size = new System.Drawing.Size(50, 27);
+            this.btnvercontra.TabIndex = 8;
+            this.btnvercontra.Text = "Ver";
+            this.btnvercontra.UseVisualStyleBackColor = true;
+            this.btnvercontra.Click += new System.EventHandler(this.btnvercontra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 308);
+            this.Controls.Add(this.btnvercontra);
+            this.Controls.Add(this.btnsalir);
+            this.Controls.Add(this.btningresar);
+            this.Controls.Add(this.txtcontrasena);
+            this.Controls.Add(this.txtusuarios);
+            this.Controls.Add(this.lblerror);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblerror;
+        private System.Windows.Forms.TextBox txtusuarios;
+        private System.Windows.Forms.TextBox txtcontrasena;
+        private System.Windows.Forms.Button btningresar;
+        private System.Windows.Forms.Button btnsalir;
+        private System.Windows.Forms.Button btnvercontra;
+    }
+}
+
---
+++ ProyectoDeProgramacionyBaseDeDatos/ProyectoDeProgramacionyBaseDeDatos/Form1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace ProyectoDeProgramacionyBaseDeDatos
+{
+    public partial class Form1 : Form
+    {
+        static string conexionstring = "server = localhost; database = ProyectobaseProgra; integrated security = true";
+        SqlConnection conexion = new SqlConnection(conexionstring);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void login()
+        {
+
+            string contodo = "exec validar '" + txtusuarios.Text + "', '" + txtcontrasena.Text + "'";
+            SqlCommand commantodo = new SqlCommand(contodo, conexion);
+            SqlDataReader todo = commantodo.ExecuteReader();
+
+
+            if (todo.Read())
+            {
+                Form2 hola = new Form2();
+                hola.Show();
+                this.Hide();
+            }
+            else
+            {
+                conexion.Close();
+                lblerror.Text = "Contraseña o Usuario Incorrectos";
+            }
+        }
+
+        private void btningresar_Click(object sender, EventArgs e)
+        {
+            conexion.Open();
+            login();
+        }
+
+        private void btnsalir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnvercontra_Click(object sender, EventArgs e)
+        {
+            if (txtcontrasena.PasswordChar == '*')
+            {
+                txtcontrasena.PasswordChar = '\0';
+            }
+            else
+            {
+                txtcontrasena.PasswordChar = '*';
+            }
+        }
+    }
+}
